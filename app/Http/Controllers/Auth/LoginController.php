a8a2391425b92a327154abe021bd7159bd2fd805
Rajesh Reddy <rajeshnecg@gmail.com>
2017-02-11 01:26:53 -0500

    override authenticatesusers trait to include flash messages

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Session;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,40 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
+    }
+
+    /** Override the Authenticated and Logout methods which are under AuthenticatesUsers
+        use Session and Illuminate\Http\Request namespaces
+    */
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        Session::flash('alert-success', 'Welcome back ' . $user->first_name . ', you have been logged in');
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->flush();
+
+        $request->session()->regenerate();
+
+        Session::flash('alert-danger', 'You have been logged out successfully');
+
+        return redirect('/');
     }
 }
