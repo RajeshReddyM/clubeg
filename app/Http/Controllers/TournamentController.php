f38d902f2a704b6458bf4a26e4f8ff5b362150bc
Selleal <trem0232@algonquinlive.com>
2017-03-06 14:03:19 -0500

    Forgot to commit Tournament Controller

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Tournament;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Auth;
+use Session;
 
 class TournamentController extends Controller
 {
@@ -23,24 +27,45 @@ class TournamentController extends Controller
      */
 
     /*Created: 2017-02-16 - Michel Tremblay
-     *Updated - 2017-02-17 - Michel Tremblay
-     ***returning array of placeholder tournament information
      * Controller function which returns the basic view with tournament data which the user requests
-     * TODO: Add functionality to fetch and return tournament data
+     * TODO: Once all tables are complete - restrict the query to only grab tournaments available to this user
     */
-    public function view()
+    public function index()
     {
-        $tournamentInfo = ['start_date'=>'2017-02-27', 'golf_club_name'=>'Candy Land', 'golf_course_name'=>'Free Labour Range','tournament_type'=>'Private'];
-        return view('tournamentRegistration')->with(compact('tournamentInfo'));
+        $tournaments = DB::table('tournaments')->get();
+        return view('tournaments/index')->with('tournaments', $tournaments);
+    }
+
+    /*Created: 2017-02-16 - Michel Tremblay
+     * Controller function which handles the page request to create a new tournament
+    */
+    public function create()
+    {
+        $tournament = new Tournament;
+
+        return view('users.create')->with(compact('tournament'));
+    }
+
+    /*Created: 2017-02-16 - Michel Tremblay
+     * Controller function which returns the basic view with tournament data which the user requests
+    */
+    public function view($tournamentId)
+    {
+        //grab the required tournament
+        //TODO: Create a way to get a list of players for a tournament  { Users - Tournaments table }
+        $tournamentInfo =  DB::table('tournaments')->where('id', $tournamentId)->first();
+        $courseInfo = DB::table('golfcourses')->where('id',$tournamentId->course_id)->first();
+
+        return view('tournaments/register')->with('tournamentInfo', $tournamentInfo);
     }
 
 
     /*Created: 2017-02-16 - Michel Tremblay
     * Controller function which registers the user for the selected tournament
-    * TODO: Add functionality to fetch and return tournament data
+    * TODO: Add functionality to register the current user in a tournament. This function should check if the user has access to the tournament before registering
    */
-    public function register()
+    public function register($tournamentInfo)
     {
-        return view('tournamentRegistration');
+        return view('tournaments/register');
     }
 }
