f63b994cf60154eb30fb762086dcbe52232b2a19
Selleal <trem0232@algonquinlive.com>
2017-04-06 21:25:43 -0400

    Updated tournament index view
    Admins are the only people that can create tournaments now

--- resources/views/tournaments/index.blade.php
+++ resources/views/tournaments/index.blade.php
@@ -9,11 +9,13 @@
             <div class="col-md-3">
                 <h3 class="title">{{trans('tournaments.all_tournaments')}}</h3>
             </div>
+            @if (Auth::user()->isAn('admin'))
             <div class="col-md-2">
                 <a href="{{ action('TournamentsController@create') }}" class="btn btn-success addButton">
                   <i class="glyphicon glyphicon-plus" aria-hidden="true"> </i> {{trans('tournaments.add_tournament')}}
                 </a>
             </div>
+            @endif
         </div>
         <hr/>
         @foreach($tournaments as $tournament)
