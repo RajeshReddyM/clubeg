c607eabd85f289c83ae9417cfae63af6e440b299
pinkninjajess <jessica.valenti1@gmail.com>
2017-04-07 23:09:53 -0400

    Edited translations

--- resources/lang/fr/golf_club.php
+++ resources/lang/fr/golf_club.php
@@ -2,27 +2,27 @@
 
 return [
     /* Golf Club - create.blade.php & edit.blade.php */
-    'add_club' => 'Ajouter Club',
+    'add_club' => 'Ajouter un club',
     'name' => 'Nom',
-    'street_no' => 'Le Numéro De La Rue',
-    'street_name' => 'Nom De La Rue',
+    'street_no' => 'Le numéro de la rue',
+    'street_name' => 'Nom de la rue’,
     'city' => 'Ville',
     'province' => 'Province',
-    'postal_code' => 'Code Postal',
+    'postal_code' => 'Code postal',
     'logo' => 'Logo',
     'submit' => 'Soumettre',
 
     'editing_club' => 'Édition Du Club',
 
     /* Golf Club - index.blade.php */
-    'golf_clubs' => 'Les Clubs De Golf',
-    'club_place_image' => 'Club espace réservé d\'image',
+    'golf_clubs' => 'Les clubs de golf',
+    'club_place_image' => ‘Paramètre fictif réservé pour un image du club’,
 
     /* Golf Club - show.blade.php */
-    'golf_courses' => 'Les Terrains De Golf',
+    'golf_courses' => 'Les terrains de golf',
 
     /* Golf Club - GolfClubController.php */
     'create_golfclub' => 'Créé avec succès golf club',
-    'update_golfclub' => 'Club de Golf mis à jour avec Succès',
-    'delete_golfclub' => 'Club de Golf supprimé avec Succès',
+    'update_golfclub' => ‘Le club de golf a été mis à jour avec succès',
+    'delete_golfclub' => ‘Le club de golf a été supprimé avec succès',
 ];
