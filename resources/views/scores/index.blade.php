25fbb3fb9c7c4c47b320ca84d730d44829b7ca66
Rajesh Reddy <rajeshnecg@gmail.com>
2017-04-09 19:00:53 -0400

    center align text

--- resources/views/scores/index.blade.php
+++ resources/views/scores/index.blade.php
@@ -25,77 +25,77 @@
       <table class="table table-responsive table-bordered">
           <thead>
             <tr>
-              <td>Hole</td>
+              <td class="text-center">Hole</td>
               @for ($i=1; $i<10; $i++)
-                <td> {{ $i }} </td>
+                <td class="text-center"> {{ $i }} </td>
               @endfor
-              <td>OUT</td>
+              <td class="text-center">OUT</td>
               @for ($i=10; $i<19; $i++)
-                <td> {{ $i }} </td>
+                <td class="text-center"> {{ $i }} </td>
               @endfor
-              <td>IN</td>
-              <td>Total</td>
-              <td>+/-</td>
+              <td class="text-center">IN</td>
+              <td class="text-center">Total</td>
+              <td class="text-center">+/-</td>
             </tr>
             <tr>
-              <td>Par</td>
-              <td>5</td>
-              <td>3</td>
-              <td>5</td>
-              <td>3</td>
-              <td>4</td>
-              <td>3</td>
-              <td>5</td>
-              <td>4</td>
-              <td>4</td>
-              <td>36</td>
-              <td>5</td>
-              <td>4</td>
-              <td>4</td>
-              <td>3</td>
-              <td>5</td>
-              <td>4</td>
-              <td>3</td>
-              <td>4</td>
-              <td>4</td>
-              <td>36</td>
-              <td>72</td>
-              <td></td>
+              <td class="text-center">Par</td>
+              <td class="text-center">5</td>
+              <td class="text-center">3</td>
+              <td class="text-center">5</td>
+              <td class="text-center">3</td>
+              <td class="text-center">4</td>
+              <td class="text-center">3</td>
+              <td class="text-center">5</td>
+              <td class="text-center">4</td>
+              <td class="text-center">4</td>
+              <td class="text-center">36</td>
+              <td class="text-center">5</td>
+              <td class="text-center">4</td>
+              <td class="text-center">4</td>
+              <td class="text-center">3</td>
+              <td class="text-center">5</td>
+              <td class="text-center">4</td>
+              <td class="text-center">3</td>
+              <td class="text-center">4</td>
+              <td class="text-center">4</td>
+              <td class="text-center">36</td>
+              <td class="text-center">72</td>
+              <td class="text-center"></td>
             </tr>
           </thead>
           <tbody>
               @foreach ($scores as $score)
                 <tr>
-                  <?php echo "<td><b>". implode("<br/>",array_map('ucfirst', $score->teamusers())). "</b></td>" ?>
+                  <?php echo "<td class='text-center'><b>". implode("<br/>",array_map('ucfirst', $score->teamusers())). "</b></td>" ?>
                   @for ($i=1; $i<=9; $i++)
                     @if ($score['H'.$i] === $score->golfcourse['P'.$i] - 1)
-                      <td class="birdie"> {{$score['H'.(string)$i]}} </td>
+                      <td class="birdie text-center"> {{$score['H'.(string)$i]}} </td>
                     @elseif ($score['H'.$i] === $score->golfcourse['P'.$i] - 2)
-                      <td class="eagle"> {{$score['H'.(string)$i]}} </td>
+                      <td class="eagle text-center"> {{$score['H'.(string)$i]}} </td>
                     @elseif ($score['H'.$i] === $score->golfcourse['P'.$i] + 1)
-                      <td class="bogey"> {{$score['H'.(string)$i]}} </td>
+                      <td class="bogey text-center"> {{$score['H'.(string)$i]}} </td>
                     @elseif ($score['H'.$i] === $score->golfcourse['P'.$i] + 2)
-                      <td class="double"> {{$score['H'.(string)$i]}} </td>
+                      <td class="double text-center"> {{$score['H'.(string)$i]}} </td>
                     @elseif ($score['H'.$i] === $score->golfcourse['P'.$i])
-                      <td> {{$score['H'.(string)$i]}} </td>
+                      <td class="text-center"> {{$score['H'.(string)$i]}} </td>
                     @else
-                      <td class="other"> {{$score['H'.(string)$i]}} </td>
+                      <td class="other text-center"> {{$score['H'.(string)$i]}} </td>
                     @endif
                   @endfor
                   <td> 36 </td>
                   @for ($i=10; $i<=18; $i++)
                     @if ($score['H'.$i] === $score->golfcourse['P'.$i] - 1)
-                      <td class="birdie"> {{$score['H'.(string)$i]}} </td>
+                      <td class="birdie text-center"> {{$score['H'.(string)$i]}} </td>
                     @elseif ($score['H'.$i] === $score->golfcourse['P'.$i] - 2)
-                      <td class="eagle"> {{$score['H'.(string)$i]}} </td>
+                      <td class="eagle text-center"> {{$score['H'.(string)$i]}} </td>
                     @elseif ($score['H'.$i] === $score->golfcourse['P'.$i]+ 1)
-                      <td class="bogey"> {{$score['H'.(string)$i]}} </td>
+                      <td class="bogey text-center"> {{$score['H'.(string)$i]}} </td>
                     @elseif ($score['H'.$i] === $score->golfcourse['P'.$i] + 2)
-                      <td class="double"> {{$score['H'.(string)$i]}} </td>
+                      <td class="double text-center"> {{$score['H'.(string)$i]}} </td>
                     @elseif ($score['H'.$i] === $score->golfcourse['P'.$i])
-                      <td> {{$score['H'.(string)$i]}} </td>
+                      <td class="text-center"> {{$score['H'.(string)$i]}} </td>
                     @else
-                      <td class="other"> {{$score['H'.(string)$i]}} </td>
+                      <td class="other text-center"> {{$score['H'.(string)$i]}} </td>
                     @endif
                   @endfor
                   <td> 36 </td>
