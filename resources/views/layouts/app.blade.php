dddca1a66e16c7e4d43bd03d139feec97123725b
Rajesh Reddy <rajeshnecg@gmail.com>
2017-04-13 21:31:34 -0400

    view scores for the selected tournament

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -129,20 +129,21 @@
                             <li ><a class="dropdown-item" href="{{ route('groups.index') }}">{{trans('app.groups')}}<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-list"></span></a></li>
                             <li ><a class="dropdown-item" href="{{ route('teams.index') }}">Teams<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-list"></span></a></li>
                             @if (Auth::user()->isAn('admin', 'scorer'))
-                                <li ><a class="dropdown-item" href="{{ route('scores.index') }}">{{trans('app.scores')}}<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-list"></span></a></li>
+                                <li ><a class="dropdown-item" href="/listtournaments"> Scores <span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-list"></span></a></li>
+                                <li ><a class="dropdown-item" href="{{ route('scores.create') }}"> Add Score <span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-plus"></span></a></li>
                             @endif
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings<span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
-                                <ul class="dropdown-menu forAnimate" role="menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">Separated link</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">One more separated link</a></li>
-                                </ul>
-                            </li>
+                            @if (Auth::user()->isAn('admin'))
+                                <li class="dropdown">
+                                    <a class="dropdown-item" href="#" class="dropdown-toggle" data-toggle="dropdown">Settings<span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
+                                    <ul class="dropdown-menu forAnimate" role="menu">
+                                        <li><a href="#">Some Link</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Separated link</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">One more separated link</a></li>
+                                    </ul>
+                                </li>
+                            @endif
                         </ul>
                     </div>
                 </div>
