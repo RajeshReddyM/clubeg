9ed8b0fd70336f75c08cd04ebd2da0ff0ee24466
pinkninjajess <jessica.valenti1@gmail.com>
2017-04-16 21:53:25 -0400

    Trans edits

--- resources/lang/fr/golf_courses.php
+++ resources/lang/fr/golf_courses.php
@@ -1,18 +1,18 @@
-<?php
+﻿<?php
 
 return [
-    'golf_course' => 'Parcours De Golf',
-    'add_golf_courses' => 'Ajouter Les Parcours De Golf',
-    'hole_no' => 'Nombre De Trous',
-    'hole_length' => 'Longueur Du Trou',
-    'par' => 'Par',
-    'select_club' => 'Sélectionnez un Club...',
-    'golf_course_place_img' => 'Le Parcours de Golf de l\'espace réservé d\'image',
-    'edit_golf_course' => 'L\'Édition De Parcours De Golf',
-    'no_golfcourses' => 'Pas de Parcours de Golf trouvé',
+    'golf_course' => 'Parcours de golf',
+    'add_golf_courses' => 'Ajouter les parcours de golf',
+    'hole_no' => 'Nombre de trous',
+    'hole_length' => 'Longueur du trou',
+    'par' => 'Normale',
+    'select_club' => 'Sélectionner un club...',
+    'golf_course_place_img' => 'Paramètre fictif réservé pour un image du parcour de golf',
+    'edit_golf_course' => 'Modification du parcours de golf',
+    'no_golfcourses' => 'Aucun parcours de golf correspondant',
 
     /* Golf Course - GolfCourseController.php */
-    'create_golfcourse' => 'Créé avec succès le parcours de golf',
-    'update_golfcourse' => 'Parcours de Golf mis à jour avec Succès',
-    'delete_golfcourse' => 'Parcours de Golf supprimé avec Succès',
+    'create_golfcourse' => 'Le parcours de golf a été créé avec succès',
+    'update_golfcourse' => 'Le parcours de golf a été mis à jour avec succès',
+    'delete_golfcourse' => 'Le parcours de golf a été supprimé avec succès',
 ];
